effa54dcf48b1ff2d37690504cedf4ec855804bf
a-szulc <adamszulc17@gmail.com>
2024-02-15 23:35:04 +0100

    some more remaps

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -15,11 +15,15 @@ return {
         },
       })
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       
-      vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
       vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+
+      -- find string
+      vim.keymap.set("n", "<leader>fs", builtin.live_grep, {})
+
+      -- find under cursor
+      vim.keymap.set("n", "<leader>fu", builtin.grep_string, {})
 
       require("telescope").load_extension("ui-select")
     end,
--- nvim/.config/nvim/lua/remap.lua
+++ nvim/.config/nvim/lua/remap.lua
@@ -2,6 +2,11 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>af", ":Explore<CR>")
 
+-- buffer controls
+vim.keymap.set("n", "<leader>q", ":bdelete<CR>")
+vim.keymap.set("n", "[b", ":bprevious<CR>")
+vim.keymap.set("n", "]b", ":bnext<CR>")
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
