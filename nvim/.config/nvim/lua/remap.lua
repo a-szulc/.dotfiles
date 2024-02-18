307adcde1a26b8d2acfacd535cbe16585e05e644
a-szulc <adamszulc17@gmail.com>
2024-02-18 18:04:11 +0100

    nvim treesitter bash and undo remap

--- nvim/.config/nvim/lua/remap.lua
+++ nvim/.config/nvim/lua/remap.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>af", ":Explore<CR>")
+
+vim.keymap.set("n", "U", "<C-R>")
 
 -- buffer controls
 -- vim.keymap.set("n", "<leader>q", ":bdelete<CR>")
