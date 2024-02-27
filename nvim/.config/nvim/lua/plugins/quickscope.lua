9bbc35ac07e9524d725119bff9dbefb0688a0585
a-szulc <adamszulc17@gmail.com>
2024-02-27 16:17:46 +0100

    quick scope plugin in nvim
    added first quickscope configuraiton

---
+++ nvim/.config/nvim/lua/plugins/quickscope.lua
@@ -0,0 +1,15 @@
+return {
+	"unblevable/quick-scope",
+	init = function()
+		-- Trigger a highlight in the appropriate direction when pressing these keys:
+		--vim.g.qs_highlight_on_keys = { "f", "F", "t", "T" }
+
+		vim.cmd([[
+        augroup qs_colors
+            autocmd!
+            autocmd ColorScheme * highlight QuickScopePrimary guifg='#afff5f' gui=underline cterm=underline
+            autocmd ColorScheme * highlight QuickScopeSecondary guifg='#5fffff' gui=underline cterm=underline
+        augroup END
+        ]])
+	end,
+}
