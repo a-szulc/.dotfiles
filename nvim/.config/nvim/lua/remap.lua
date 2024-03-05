edf42207ab5bf2884df56bd9f91e675c4cb03b6c
a-szulc <adamszulc17@gmail.com>
2024-03-05 15:13:19 +0100

    harpoon
    added harpoon to nvim and some miscelionious changes :DDDD

--- nvim/.config/nvim/lua/remap.lua
+++ nvim/.config/nvim/lua/remap.lua
@@ -21,6 +21,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+vim.keymap.set("n", "<C-/>", ":nohl<CR>")
+
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
