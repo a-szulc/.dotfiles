491367370cf263232e3026e30d13b035d3830852
a-szulc <adamszulc17@gmail.com>
2024-03-24 00:10:45 +0100

    added theme switcher functionality
    now need to add another waybar theme to fit fast hyprland theme

--- hypr/.config/hypr/hyprscripts/switch-theme.sh
+++ hypr/.config/hypr/hyprscripts/switch-theme.sh
@@ -1,3 +1,37 @@
 #!/bin/bash
 
-bat ~/.config/hypr/themes
+# bat ~/.dotfiles/hypr/.config/hypr/themes
+
+current_theme=$(cat ~/.cache/hyprland_current_theme 2>/dev/null)
+
+theme_list=($(find ~/.dotfiles/hypr/.config/hypr/themes/ -maxdepth 1 -type f -exec basename {} \;))
+
+# to print whole list
+#echo ${theme_list[*]}
+# to print length of list
+#echo ${#theme_list[@]}
+echo ${current_theme}
+
+index=-1
+for ((i=0; i<${#theme_list[@]}; i++)); do
+    if [[ ${theme_list[$i]} == $current_theme ]]; then
+        index=$i
+        break
+    fi
+done
+
+next_index=$((index + 1))
+
+if [[ $index == -1 || $next_index -ge ${#theme_list[@]} ]]; then
+    next_index=0
+fi
+
+next_theme=${theme_list[$next_index]}
+
+echo $next_theme > ~/.cache/hyprland_current_theme
+
+echo ${next_theme}
+#cat ~/.dotfiles/hypr/.config/hypr/themes/${next_theme}
+cat ~/.dotfiles/hypr/.config/hypr/themes/${next_theme} > ~/.dotfiles/hypr/.config/hypr/current_theme.conf
+hyprctl reload
+
