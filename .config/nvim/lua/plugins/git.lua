443e40d797df3714f8a553233a429b1ffa899057
a-szulc <adamszulc17@gmail.com>
2024-07-05 15:07:24 +0200

    changed git hunk bind and tmux date display

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -7,7 +7,7 @@ return {
     config = function()
       require("gitsigns").setup()
 
-      vim.keymap.set("n", "<leader>gd", ":Gitsigns preview_hunk<CR>", {})
+      vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", {})
       vim.keymap.set("n", "<leader>gi", ":Gitsigns preview_hunk_inline<CR>", {})
       vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
     end
