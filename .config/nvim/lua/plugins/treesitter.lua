b6f7d4f8ea7fcbef4eab109d7b653093779fe730
a-szulc <adamszulc17@gmail.com>
2024-07-24 15:31:13 +0200

    add treesitter contex

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,27 @@
 return {
     {
+        "nvim-treesitter/nvim-treesitter-context",
+        config = function()
+            require("treesitter-context").setup({
+                max_lines = 5,
+                min_window_height = 15,
+                multiline_threshold = 1,
+            })
+
+            vim.keymap.set("n", "<leader>ct", ":TSContextToggle<CR>")
+        end,
+    },
+    {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         config = function()
             local config = require("nvim-treesitter.configs")
             config.setup({
-                ensure_installed = {"css", "c", "lua", "bash", "markdown", "markdown_inline"}, -- "rust", "go", "java", "javascript", "typescript"},
+                ensure_installed = { "css", "c", "lua", "bash", "markdown", "markdown_inline" }, -- "rust", "go", "java", "javascript", "typescript"},
                 auto_install = true,
                 highlight = { enable = true },
                 indent = { enable = true },
             })
-        end
-    }
+        end,
+    },
 }
