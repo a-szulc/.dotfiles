bb496a2b94f754ad5b672f8c96be43a3276f38e6
a-szulc <adamszulc17@gmail.com>
2024-08-16 15:27:06 +0200

    make those binds work like intended

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -9,9 +9,10 @@ return {
 
             vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", {})
             vim.keymap.set("n", "<leader>gi", ":Gitsigns preview_hunk_inline<CR>", {})
+
             vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
-            vim.keymap.set("n", "<leader>[g", ":Gitsigns prev_hunk<CR>", {})
-            vim.keymap.set("n", "<leader>]g", ":Gitsigns next_hunk<CR>", {})
+            vim.keymap.set("n", "[g", ":Gitsigns prev_hunk<CR>", {})
+            vim.keymap.set("n", "]g", ":Gitsigns next_hunk<CR>", {})
         end,
     },
 }
