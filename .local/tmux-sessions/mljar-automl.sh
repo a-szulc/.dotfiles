bb98508a90dd1a578102d1dccb90931b72803710
a-szulc <adamszulc17@gmail.com>
2024-08-26 15:05:07 +0200

    update automl sessions

--- .local/tmux-sessions/mljar-automl.sh
+++ .local/tmux-sessions/mljar-automl.sh
@@ -7,7 +7,18 @@ if [ $? != 0 ]
  then
 
     tmux new-session -s $SESSIONNAME -c ~/mljar/mljar-supervised -d -n nvim
+    tmux send-keys "source venv/bin/activate" Enter
     tmux send-keys "v ." Enter
+
+    tmux new-window -c ~/mljar/mljar-supervised -n pytest
+    tmux send-keys "source venv/bin/activate" Enter
+    tmux send-keys "pytest tests/ -v"
+
+    tmux new-window -c ~/mljar/mljar-supervised -n errors
+    tmux send-keys "nvim automl_tests/errors.md"
+
+    tmux new-window -c ~/mljar/mljar-supervised -n failed_tests
+    tmux send-keys "nvim failed_tests"
 
     tmux new-window -c ~/mljar/mljar-supervised -n lazygit
     tmux send-keys "lazygit" Enter
