4f761b647e0e56209747238e1727a173e904b815
a-szulc <adamszulc17@gmail.com>
2024-12-17 22:27:47 +0100

    add jumping in nvim snippets

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -1,81 +1,98 @@
 return {
-	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
+    {
+        "hrsh7th/cmp-nvim-lsp",
+    },
+    {
+        "L3MON4D3/LuaSnip",
+        dependencies = {
+            "saadparwaiz1/cmp_luasnip",
+            "rafamadriz/friendly-snippets",
+        },
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        config = function()
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+            require("luasnip.loaders.from_vscode").lazy_load()
 
-			cmp.setup({
+            cmp.setup({
 
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
 
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
 
-				-- mapping = cmp.mapping.preset.insert({
-				-- 	["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				-- 	["<C-f>"] = cmp.mapping.scroll_docs(4),
-				-- 	["<C-Space>"] = cmp.mapping.complete(),
-				-- 	["<C-e>"] = cmp.mapping.abort(),
-				-- 	["<CR>"] = cmp.mapping.confirm({ select = true }),
-				-- 	["<Tab>"] = cmp.mapping(function(fallback)
-				-- 		if cmp.visible() then
-				-- 			cmp.select_next_item()
-				-- 			-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-				-- 			-- that way you will only jump inside the snippet region
-				-- 		else
-				-- 			fallback()
-				-- 		end
-				-- 	end, { "i", "s" }),
-				-- 	["<S-Tab>"] = cmp.mapping(function(fallback)
-				-- 		if cmp.visible() then
-				-- 			cmp.select_prev_item()
-				-- 		else
-				-- 			fallback()
-				-- 		end
-				-- 	end, { "i", "s" }),
-				-- }),
+                -- mapping = cmp.mapping.preset.insert({
+                -- 	["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                -- 	["<C-f>"] = cmp.mapping.scroll_docs(4),
+                -- 	["<C-Space>"] = cmp.mapping.complete(),
+                -- 	["<C-e>"] = cmp.mapping.abort(),
+                -- 	["<CR>"] = cmp.mapping.confirm({ select = true }),
+                -- 	["<Tab>"] = cmp.mapping(function(fallback)
+                -- 		if cmp.visible() then
+                -- 			cmp.select_next_item()
+                -- 			-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+                -- 			-- that way you will only jump inside the snippet region
+                -- 		else
+                -- 			fallback()
+                -- 		end
+                -- 	end, { "i", "s" }),
+                -- 	["<S-Tab>"] = cmp.mapping(function(fallback)
+                -- 		if cmp.visible() then
+                -- 			cmp.select_prev_item()
+                -- 		else
+                -- 			fallback()
+                -- 		end
+                -- 	end, { "i", "s" }),
+                -- }),
 
-				mapping = {
-					["<C-e>"] = cmp.mapping.abort(),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-					["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-					["<C-y>"] = cmp.mapping(
-						cmp.mapping.confirm({
-							behavior = cmp.ConfirmBehavior.Insert,
-							select = true,
-						}),
-						{ "i", "c" }
-					),
-				},
+                mapping = {
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-n>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+                        elseif luasnip.locally_jumpable(1) then
+                            luasnip.jump(1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                    ["<C-p>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
+                        elseif luasnip.locally_jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                    ["<C-y>"] = cmp.mapping(
+                        cmp.mapping.confirm({
+                            behavior = cmp.ConfirmBehavior.Insert,
+                            select = true,
+                        }),
+                        { "i", "c" }
+                    ),
+                },
 
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" }, -- For luasnip users.
-					{ name = "buffer" },
-				}),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" }, -- For luasnip users.
+                    { name = "buffer" },
+                }),
 
-				formatting = {
-					format = require("nvim-highlight-colors").format,
-				},
-			})
-		end,
-	},
+                formatting = {
+                    format = require("nvim-highlight-colors").format,
+                },
+            })
+        end,
+    },
 }
